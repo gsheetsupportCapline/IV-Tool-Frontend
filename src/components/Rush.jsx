a2d68d7c9e85540b62ec2fdfdd76a5913bf26168
Shubham Pandey <shubhampandey0806@gmail.com>
2024-07-26 13:13:09 +0530

    Changes in color

--- src/components/Rush.jsx
+++ src/components/Rush.jsx
@@ -5,7 +5,7 @@ import Header from "./Header";
 import Stack from "@mui/material/Stack";
 import Button from "@mui/material/Button";
 import Card from "@mui/material/Card";
-
+import Typography from "@mui/material/Typography";
 import { DatePicker } from "@mui/x-date-pickers/DatePicker";
 import { TimePicker } from "@mui/x-date-pickers/TimePicker";
 import axios from "axios"; // Import Axios
@@ -126,170 +126,194 @@ const Rush = () => {
   return (
     <>
       <Header />
-      <Card
+      <Box
         sx={{
-          width: "50%",
-          height: "100vh",
-          borderColor: "primary.main",
-          borderRadius: 1,
+          width: "100vw", // Full viewport width
+
+          backgroundColor: "#cbd5e1", // Set the background color
           display: "flex",
-          flexDirection: "column",
-          alignItems: "center",
-          margin: "auto", // Center the card horizontally
-          marginTop: "20px", // Adjust top margin to avoid sticking to the header
+          justifyContent: "center", // Center the Card horizontally
+          alignItems: "center", // Center the Card vertically
         }}
       >
-        <Box
-          component="form"
+        <Card
           sx={{
+            width: "50%",
+            height: "90vh",
+            borderColor: "primary.main",
+            borderRadius: 3,
             display: "flex",
             flexDirection: "column",
-            gap: 2,
-            width: "90%", // Adjust width as needed
-            padding: 2,
-            backgroundColor: "#f1f5f9",
+            alignItems: "center",
+            margin: "auto", // Center the card horizontally
+            marginTop: "10px", // Adjust top margin to avoid sticking to the header
+            backgroundColor: "#334155",
           }}
-          noValidate
-          autoComplete="off"
         >
-          <Grid container spacing={2}>
-            {/* First Column */}
-            <Grid item xs={12} sm={6}>
-              <FormControl fullWidth sx={{ marginBottom: 2 }}>
-                <InputLabel id="office-label">Office</InputLabel>
-                <Select
-                  labelId="office-label"
-                  id="office-select"
-                  value={selectedOffice}
-                  onChange={(e) => handleOfficeChange(e.target.value)}
-                  label="Office"
-                >
-                  {officeNames.map((office) => (
-                    <MenuItem key={office.id} value={office.officeName}>
-                      {office.officeName}
-                    </MenuItem>
-                  ))}
-                </Select>
-              </FormControl>
+          <Typography
+            variant="h4"
+            sx={{ marginBottom: 2, marginTop: 2, color: "white" }}
+          >
+            Rush IV
+          </Typography>
+          <Box
+            component="form"
+            sx={{
+              display: "flex",
+              flexDirection: "column",
+              gap: 2,
+              width: "90%", // Adjust width as needed
+              padding: 2,
+              backgroundColor: "#f1f5f9",
+              borderRadius: 2,
+              marginBottom: "10px",
+            }}
+            noValidate
+            autoComplete="off"
+          >
+            <Grid container spacing={2}>
+              {/* First Column */}
+              <Grid item xs={12} sm={6}>
+                <FormControl fullWidth sx={{ marginBottom: 2 }}>
+                  <InputLabel id="office-label">Office</InputLabel>
+                  <Select
+                    labelId="office-label"
+                    id="office-select"
+                    value={selectedOffice}
+                    onChange={(e) => handleOfficeChange(e.target.value)}
+                    label="Office"
+                  >
+                    {officeNames.map((office) => (
+                      <MenuItem key={office.id} value={office.officeName}>
+                        {office.officeName}
+                      </MenuItem>
+                    ))}
+                  </Select>
+                </FormControl>
 
-              <TextField
-                required
-                id="outlined-read-only-treating-provider"
-                label="Treating Provider"
-                InputProps={{
-                  readOnly: true,
-                }}
-                value={treatingProvider}
-                sx={{ marginBottom: 2 }}
-                fullWidth
-              />
-              <TextField
-                required
-                id="outlined-patient-id"
-                label="Patient Id"
-                onChange={(e) => handleChange(e.target.value, "patientId")}
-                sx={{ marginBottom: 2 }}
-                fullWidth
-              />
-              <DatePicker
-                required
-                id="outlined-patient-dob"
-                label="Patient DOB"
-                value={values.patientDOB}
-                onChange={handlePatientDOBChange}
-                sx={{ marginBottom: 2, width: "100%" }}
-                fullWidth
-              />
+                <TextField
+                  required
+                  id="outlined-read-only-treating-provider"
+                  label="Treating Provider"
+                  InputProps={{
+                    readOnly: true,
+                  }}
+                  value={treatingProvider}
+                  sx={{ marginBottom: 2 }}
+                  fullWidth
+                />
+                <TextField
+                  required
+                  id="outlined-patient-id"
+                  label="Patient Id"
+                  onChange={(e) => handleChange(e.target.value, "patientId")}
+                  sx={{ marginBottom: 2 }}
+                  fullWidth
+                />
+                <DatePicker
+                  required
+                  id="outlined-patient-dob"
+                  label="Patient DOB"
+                  value={values.patientDOB}
+                  onChange={handlePatientDOBChange}
+                  sx={{ marginBottom: 2, width: "100%" }}
+                  fullWidth
+                />
 
-              <TextField
-                required
-                id="outlined-insurance-name"
-                label="Insurance Name"
-                value={values.insuranceName}
-                onChange={(e) => handleChange(e.target.value, "insuranceName")}
-                sx={{ marginBottom: 2 }}
-                fullWidth
-              />
-              <TextField
-                id="outlined-policy-holder-name"
-                label="Policy Holder Name"
-                value={values.policyHolderName}
-                onChange={(e) =>
-                  handleChange(e.target.value, "policyHolderName")
-                }
-                sx={{ marginBottom: 2 }}
-                fullWidth
-              />
+                <TextField
+                  required
+                  id="outlined-insurance-name"
+                  label="Insurance Name"
+                  value={values.insuranceName}
+                  onChange={(e) =>
+                    handleChange(e.target.value, "insuranceName")
+                  }
+                  sx={{ marginBottom: 2 }}
+                  fullWidth
+                />
+                <TextField
+                  id="outlined-policy-holder-name"
+                  label="Policy Holder Name"
+                  value={values.policyHolderName}
+                  onChange={(e) =>
+                    handleChange(e.target.value, "policyHolderName")
+                  }
+                  sx={{ marginBottom: 2 }}
+                  fullWidth
+                />
+              </Grid>
+
+              {/* Second Column */}
+              <Grid item xs={12} sm={6}>
+                <DatePicker
+                  required
+                  id="outlined-aptDate"
+                  label="Appointment Date"
+                  renderInput={(params) => <TextField {...params} />}
+                  value={values.appointmentDate}
+                  onChange={handleAppointmentDateChange}
+                  sx={{ marginBottom: 2, width: "100%" }}
+                  fullWidth
+                />
+                <TimePicker
+                  label="Appointment Time"
+                  renderInput={(params) => <TextField {...params} />}
+                  value={values.appointmentTime}
+                  onChange={handleTimeChange}
+                  sx={{ marginBottom: 2, width: "100%" }}
+                  fullWidth
+                />
+
+                <TextField
+                  required
+                  id="outlined-patient-name"
+                  label="Patient Name"
+                  value={values.patientName}
+                  onChange={(e) => handleChange(e.target.value, "patientName")}
+                  sx={{ marginBottom: 2 }}
+                  fullWidth
+                />
+                <TextField
+                  required
+                  id="outlined-mid-ssn"
+                  label="MID/SSN"
+                  value={values.MIDSSN}
+                  onChange={(e) => handleChange(e.target.value, "MIDSSN")}
+                  sx={{ marginBottom: 2 }}
+                  fullWidth
+                />
+
+                <TextField
+                  required
+                  id="outlined-insurance-contact"
+                  label="Insurance Contact"
+                  value={values.insurancePhone}
+                  onChange={(e) =>
+                    handleChange(e.target.value, "insurancePhone")
+                  }
+                  sx={{ marginBottom: 2 }}
+                  fullWidth
+                />
+
+                <DatePicker
+                  id="outlined-policy-holder-dob"
+                  label="Policy Holder DOB"
+                  value={values.policyHolderDOB}
+                  onChange={handlePolicyHolderDOBChange}
+                  sx={{ marginBottom: 2, width: "100%" }}
+                  fullWidth
+                />
+              </Grid>
             </Grid>
 
-            {/* Second Column */}
-            <Grid item xs={12} sm={6}>
-              <DatePicker
-                required
-                id="outlined-aptDate"
-                label="Appointment Date"
-                renderInput={(params) => <TextField {...params} />}
-                value={values.appointmentDate}
-                onChange={handleAppointmentDateChange}
-                sx={{ marginBottom: 2, width: "100%" }}
-                fullWidth
-              />
-              <TimePicker
-                label="Appointment Time"
-                renderInput={(params) => <TextField {...params} />}
-                value={values.appointmentTime}
-                onChange={handleTimeChange}
-                sx={{ marginBottom: 2, width: "100%" }}
-                fullWidth
-              />
-
-              <TextField
-                required
-                id="outlined-patient-name"
-                label="Patient Name"
-                value={values.patientName}
-                onChange={(e) => handleChange(e.target.value, "patientName")}
-                sx={{ marginBottom: 2 }}
-                fullWidth
-              />
-              <TextField
-                required
-                id="outlined-mid-ssn"
-                label="MID/SSN"
-                value={values.MIDSSN}
-                onChange={(e) => handleChange(e.target.value, "MIDSSN")}
-                sx={{ marginBottom: 2 }}
-                fullWidth
-              />
-
-              <TextField
-                required
-                id="outlined-insurance-contact"
-                label="Insurance Contact"
-                value={values.insurancePhone}
-                onChange={(e) => handleChange(e.target.value, "insurancePhone")}
-                sx={{ marginBottom: 2 }}
-                fullWidth
-              />
-
-              <DatePicker
-                id="outlined-policy-holder-dob"
-                label="Policy Holder DOB"
-                value={values.policyHolderDOB}
-                onChange={handlePolicyHolderDOBChange}
-                sx={{ marginBottom: 2, width: "100%" }}
-                fullWidth
-              />
-            </Grid>
-          </Grid>
-
-          <Stack spacing={2} direction="row">
-            <Button variant="contained" onClick={handleSubmit}>
-              Submit
-            </Button>
-          </Stack>
-        </Box>
-      </Card>
+            <Stack spacing={2} direction="row">
+              <Button variant="contained" onClick={handleSubmit} fullWidth>
+                Submit
+              </Button>
+            </Stack>
+          </Box>
+        </Card>
+      </Box>
     </>
   );
 };
