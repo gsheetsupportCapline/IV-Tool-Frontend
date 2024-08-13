583187a99f7378968cf4ef61d579464cda4eba78
Shubham Pandey <shubhampandey0806@gmail.com>
2024-08-13 22:16:30 +0530

    added logs in Header

--- src/config/apiConfig.js
+++ src/config/apiConfig.js
@@ -1,5 +1,3 @@
 // For vite 
-//const BASE_URL =  import.meta.env.REACT_APP_API_URL
-//  const BASE_URL = "http://localhost:7478";
-const BASE_URL = "https://testing.ivtools.caplineservices.in"
+const BASE_URL = import.meta.env.VITE_REACT_APP_API_URL
 export default BASE_URL;
