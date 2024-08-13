dc83aa57120d61cdf91b91905922f721121f6147
Shubham Pandey <shubhampandey0806@gmail.com>
2024-08-13 20:37:41 +0530

    changes done

--- src/config/apiConfig.js
+++ src/config/apiConfig.js
@@ -1,4 +1,4 @@
 // For vite 
-//const BASE_URL =  import.meta.env.REACT_APP_API_URL
- const BASE_URL = "http://localhost:7478";
+const BASE_URL =  import.meta.env.REACT_APP_API_URL
+ 
 export default BASE_URL;
